9d3a1836e136b3acc07dce3313ec6181c52ef33a
Leslieiscool <963365930@qq.com>
2019-06-11 18:06:07 +0800

    de

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,8 +1,29 @@
 require(['./config'], ()=>{
-    require(['header', 'footer'],(header, footer)=>{
+    require(['swiper', 'header', 'footer'],(Swiper, header, footer)=>{
         class Detail {
             constructor(){
+                this.swiperPt();
+            }
 
+            swiperPt(){
+                let myswiper = new Swiper('.swiper-container', {
+                    speed: 500,
+                    autoplay: {
+                        autoplay: 1000,
+                        disableOnInteraction: false,
+                      },
+                    effect : 'slide',
+                    loop: true,
+                    pagination: {
+                        el: '.swiper-pagination',
+                        type: 'bullets',
+                        clickable: 'true',
+                      },
+                      navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                      },
+                })
             }
 
         }
--- src/js/login.js
+++ src/js/login.js
@@ -1,11 +1,23 @@
 require(['./config'], ()=>{
-    require(['header', 'footer'], (header,footer)=>{
+    require(['cookie', 'header', 'footer'], (cookie, header, footer)=>{
         class List {
             constructor(){
+                this.onLog();
                 
             }
             
-            
+            //点击登录事件
+            onLog(){
+                $('#logbtn').on('click', ()=>{
+                    this.saveCk();
+                })
+            }
+            // 把用户名和密码 存到cookie
+            saveCk(){
+                let username = $('#username').val();
+                let password = $('#password').val()
+                console.log(username)
+            }
         }
         new List();
     })
