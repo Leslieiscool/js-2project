0459235889dea9e7c6e3f634516b2160080c1f0e
Leslieiscool <963365930@qq.com>
2019-06-14 15:45:13 +0800

    delete

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,7 +4,7 @@ require(['./config'], ()=>{
             constructor(){
                 this.n =0;
                 this.getgd();
-                this.iptcheck();
+                
             }
 
             //从storage里取数据并渲染购物车
@@ -16,6 +16,7 @@ require(['./config'], ()=>{
                 });
                 $('#gdlist').html(html);
                 this.deletegd();
+                this.iptcheck();
             }
 
             //全选按钮
@@ -30,14 +31,20 @@ require(['./config'], ()=>{
                     // console.log($('.check').length);
                     this.n = $('.allck').is(':checked') ? $('.check').length : 0 ;
                     // this.checks($n);
+                    console.log(1);
+                    console.log(this.n);
                 });
                 this.checks();
             }
             //单选按钮
             checks(){
+                console.log($('.check').length);
+                
                 $('.check').each((index, item)=>{
+                    
                     $(item).on('change',()=>{
                         this.n += $(item).is(':checked') ? 1 : -1;
+                        console.log(2);
                         console.log(this.n);
                         // console.log($('.check').length);
                         if(this.n === $('.check').length){
@@ -48,25 +55,53 @@ require(['./config'], ()=>{
                             $('.allck').prop('checked', false);
                         }
                     })
-                    
-                });
-            }
+                })
+            };
             //删除商品
             deletegd(){
-                
-                    $('.lidelete').each((index, item)=>{
-                        console.log($('.delete'));
-                    $(item).on('click', ()=>{
-                        console.log(index);
-                        let cartlist = JSON.parse(localStorage.getItem('cart'));
-                        cartlist.splice(index, 1);
-                        console.log(cartlist);
-                        localStorage.setItem('cart', JSON.stringify(cartlist));
-                        this.getgd();
-                        header.cartnum();
-                        })
-                        
+                let _this = this;
+                $('#gdlist').on('click', '.lidelete', function(){
+                    let cartlist = JSON.parse(localStorage.getItem('cart'));
+                    // console.log($(this).attr('id'));
+                    
+                    cartlist.forEach((item, index)=>{
+                        if(item.id == $(this).attr('id')){
+                            cartlist.splice(index, 1);
+                            localStorage.setItem('cart', JSON.stringify(cartlist));
+                            header.cartnum();
+                            $(this).parents('.eygoods').remove();
+                        }
                     })
+                    if($(this).parents('.ergoods').find('.check').is(':checked')){
+                        _this.n --;
+                    }
+                    if(_this.n === $('.check').length){
+                        // console.log(1);
+                        $('.allck').prop('checked', true);
+                    }else{
+                        // console.log(2);
+                        $('.allck').prop('checked', false);
+                    }
+                })
+
+
+                    // $('.lidelete').each((index, item)=>{
+                    //     // console.log($('.delete'));
+                    // $(item).on('click', ()=>{
+                    //     // console.log(index);
+                    //     let cartlist = JSON.parse(localStorage.getItem('cart'));
+                    //     cartlist.splice(index, 1);
+                    //     // console.log(cartlist);
+                    //     localStorage.setItem('cart', JSON.stringify(cartlist));
+                    //     if($(item).is(':checked')){
+                    //         this.n --;
+                    //     };
+                    //     console.log(3);
+                    //     console.log(this.n);
+                    //     this.getgd();
+                    //     
+                    //     })
+                    // })
                 
             }
                 
